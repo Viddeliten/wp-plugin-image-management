634c348f854471f0d544f1949126225dbfe46b45
Johanna Julén <johanna.julen@gmail.com>
2023-02-28 22:10:47 +0100

    also finding if the image was uploaded to a post

--- imageManager.php
+++ imageManager.php
@@ -10,6 +10,7 @@ Author URI: http://vidde.org
 
 namespace ViddeImageManager;
 
+// Initialize the plugin
 $im = new imageManager();
 $im->init();
 
@@ -19,19 +20,23 @@ $im->init();
 class imageManager {
     private $my_plugin_screen_name;
 
-
+    /**
+     * Initialize plugin (call to add the submenu page)
+     */
     public function init() {
         add_action('admin_menu', array($this,'add_submenu_page'));
     }
 
-
+    /**
+     * call the wordpress function to add the subpage menu and specify function for displaying the page (display_thePage)
+     */
     public function add_submenu_page() {
         $this->my_plugin_screen_name = add_submenu_page( parent_slug: "upload.php",
                         page_title: __("Manage images", "vimgr"),
                         menu_title: __("Manage images", "vimgr"),
                         capability: "manage_options", 
                         menu_slug: "manage_images",
-                        callback: array($this,'display_thePage')
+                        callback: array($this,'display_thePage') // callback is correct!
         );
     }
 
@@ -54,6 +59,9 @@ class imageManager {
         ////////////////////////////////////////
         echo '<h1>'.__("Images not attached to any post", "vimgr").'</h1>';
         if($db_attachments) {
+            echo '<form method="post">
+            <div class="attachments-wrapper">
+                    <ul class="attachments">';
             foreach ($db_attachments as $post) {
                 $posts_containing_image = array();
     
@@ -64,13 +72,28 @@ class imageManager {
                     )
                 );
                 if(empty($posts_containing_image)) {
+                    echo '<li tabindex="0" role="checkbox" aria-label="post_9478" aria-checked="false" data-id="'.$post->ID.'"
+                         class="attachment save-ready">';
                     // echo '<pre>'.print_r($posts_containing_image,1).'</pre>';
                     setup_postdata($post);
                     // the_title();
                     the_attachment_link($post->ID, fullsize: false, permalink: true);
                     // the_excerpt();
+
+                    if ( $post->post_parent ) {
+                        $post_parent = get_post( $post->post_parent );
+                        $info = array();
+                        if ( $post_parent ) {
+                            $info['uploadedToTitle'] = $post_parent->post_title ? $post_parent->post_title : __( '(no title)' );
+                            $info['uploadedToLink']  = get_edit_post_link( $post->post_parent, 'raw' );
+                        }
+                        echo 'info:<pre>'.print_r($info,1).'</pre>';
+                    }
+
+                    echo '</li>';
                 }
             }
+            echo "</ul></div></form>";
         }
     }
 }
